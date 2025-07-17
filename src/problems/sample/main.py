cb0196f70952500c0f67f43c071ee4658ded2910
nakano1122 <akito2998@gmail.com>
2025-07-17 18:05:25 +0900

    create dir structure

---
+++ src/problems/sample/main.py
@@ -0,0 +1,8 @@
+"""
+＜問題＞
+以下のコードを修正して、`main`関数が"Hello, World!"というメッセージを返すようにしてください。
+"""
+
+
+def main():
+    return {"message": None}
---
+++ src/tests/sample/main.py
@@ -0,0 +1,7 @@
+"""
+想定解をテンプレートに従って実装しましょう。
+"""
+
+
+def main():
+    return {"message": "Hello, World!"}
